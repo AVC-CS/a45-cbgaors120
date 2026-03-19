eae7214dfee1dfa6368889f0065e7e18c820f331
cbgaors120 <bgaor@students.avc.edu>
2026-03-18 22:56:21 -0700

    A45 Assignment

--- main.cpp
+++ main.cpp
@@ -11,7 +11,33 @@ int main()
     cout << "Enter three integer values: ";
     cin >> num1 >> num2 >> num3;
 
+    
+
     // TODO: Find min, max, and med among num1, num2, num3
+    if (num1 < num2 && num1 < num3){
+        if (num2 < num3){
+            min = num1; med = num2; max = num3;
+        }
+        else{
+            min = num1; med = num3; max = num2;
+        }
+
+    }
+    if (num1 < num2 && num3 < num1){
+        min = num3; med = num1; max = num2;
+    }
+    if ( !(num1 < num2) && num2 < num3){
+        if (num1 < num3){
+            min = num2; med = num1; max = num3;
+        }
+        else{
+            min = num2; med = num3; max = num1;
+        }
+    }
+    if ( !(num1 < num2) && num3 < num2){
+        num3 = min; num2 = med; num1 = max;
+    }
+    
 
     cout << min << " " << med << " " << max << endl;
 }
